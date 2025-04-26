823216240a710783b1cc89cf631fe6e7615ce28a
1mag1n33 <kopeccarson@gmail.com>
2025-04-26 17:05:06 -0400

    Added AbstractCommandManager.java

---
+++ src/main/java/com/github/clawsoftsolutions/purrfectlib/manager/AbstractCommandManager.java
@@ -0,0 +1,42 @@
+package com.github.clawsoftsolutions.purrfectlib.manager;
+
+import com.github.clawsoftsolutions.purrfectlib.enums.OptionType;
+
+/**
+ * Abstract base for command managers that maps OptionTypes to whatever representation they need.
+ */
+public abstract class AbstractCommandManager {
+
+    /**
+     * Called to map an OptionType to whatever object/representation you want.
+     * If not overridden for a specific OptionType, a warning will be shown.
+     *
+     * @param type The OptionType to map.
+     * @return The mapped object, or null if unmapped.
+     */
+    public Object mapOption(OptionType type) {
+        Object result = map(type);
+        if (result == null) {
+            handleUnmappedOption(type);
+        }
+        return result;
+    }
+
+    /**
+     * Implement this method to define how your manager maps each OptionType.
+     *
+     * @param type The OptionType.
+     * @return The mapped object or null if not handled.
+     */
+    protected abstract Object map(OptionType type);
+
+    /**
+     * Handles the case where an OptionType was not mapped.
+     * Default is to print a warning. You can override if needed.
+     *
+     * @param type The unmapped OptionType.
+     */
+    protected void handleUnmappedOption(OptionType type) {
+        System.out.println("[PurrfectLib] Warning: OptionType '" + type.name() + "' is not mapped.");
+    }
+}
